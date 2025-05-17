7e5759e8a79fb1e25411ee2176cc9d16c7e8712b
AllanDev97 <soumailiallan.as@gmail.com>
2025-05-17 13:43:17 +0400

    Page accueil

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import ApiTester from "@/components/test/ApiTester";
 
 export default function Home() {
   return (
       <main className="flex flex-col h-screen w-full justify-center items-center gap-16">
-          <ApiTester requestType="post"/>
-          <ApiTester requestType="get"/>
+
       </main>
   );
 }
