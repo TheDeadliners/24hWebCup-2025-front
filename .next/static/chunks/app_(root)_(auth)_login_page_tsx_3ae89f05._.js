25c96c6d1a8a014f0eaaeef163f6134159b249cd
AllanDev97 <soumailiallan.as@gmail.com>
2025-05-18 04:15:55 +0400

    rien a commit

--- .next/static/chunks/app_(root)_(auth)_login_page_tsx_3ae89f05._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_9bd25805._.js",
-  "static/chunks/_9b70d320._.js"
-],
-    source: "dynamic"
-});
