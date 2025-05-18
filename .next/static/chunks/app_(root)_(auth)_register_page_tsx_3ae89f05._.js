61a4878c49fb370b092872625b913b1bdcd0b45d
Thomas Epicharme <tepicharme.dev@protonmail.com>
2025-05-18 06:02:08 +0400

    delete next

--- .next/static/chunks/app_(root)_(auth)_register_page_tsx_3ae89f05._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_561f9fef._.js",
-  "static/chunks/_52dd21bc._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_(root)_(backoffice)_layout_tsx_3ae89f05._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_bf464a3e._.js",
-  "static/chunks/_9f521960._.js"
-],
-    source: "dynamic"
-});
